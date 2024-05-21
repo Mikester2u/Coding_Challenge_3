277ae8505a3c871d59f81f1966b3f0e5bc98accd
Michael Rueda <michael.a.rueda22@gmail.com>
2024-05-21 11:11:06 -0400

    Test accelerate and brake methods on Car instances

--- cc_3.js
+++ cc_3.js
@@ -1,6 +1,6 @@
 //U22562170
 
-class Car {
+class Car { //Car class with accelerate and brake functions
     constructor(make, speed) {
         this.make = make;
         this.speed = speed;
@@ -13,9 +13,24 @@ accelerate() {
 
 brake () {
     this.speed -= 5;
-    console.log(this.make + "is going at " + this.speed + "km/h");
+    console.log(this.make + " is going at " + this.speed + " km/h");
     }
-    
+
 }
 
 module.exports = Car;
+
+const car1 = new Car("BMW", 120); //test data 1
+const car2 = new Car("Mercedes", 95); //test data 2
+
+//car 1
+car1.accelerate();
+car1.brake();
+car1.accelerate();
+car1.brake();
+
+//car 2
+car2.accelerate();
+car2.brake();
+car2.accelerate();
+car2.brake();
